82ce72b135062fa1dd5a179ddb5e51de9ee5a236
Luís Assunção <46464061+assuncaolfi@users.noreply.github.com>
2021-03-31 12:44:42 -0300

    better paragraphs

--- install.sh
+++ install.sh
@@ -10,8 +10,6 @@ cp init.vim ~/.config/nvim/init.vim &&
 # vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
-       
-# Plugin
 nvim +PluginInstall +qall &&
 
 # Done
