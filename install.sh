f391eef026afd958828be0be52d27f6b1bffee71
Luís Assunção <46464061+assuncaolfi@users.noreply.github.com>
2021-03-31 12:39:08 -0300

    create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+# neovim
+git clone https://github.com/neovim/neovim &&
+cd neovim &&
+make CMAKE_BUILD_TYPE=RelWithDebInfo &&
+sudo make install &&
+cd .. &&
+cp init.vim ~/.config/nvim/init.vim &&
+
+# vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' &&
+       
+# Plugin
+nvim +PluginInstall +qall &&
+
+# Done
+echo 'Done!'
