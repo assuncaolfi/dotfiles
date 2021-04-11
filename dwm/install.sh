dbce10847fb40ae4387c70350f8b917e1e3179f5
assuncaolfi <assuncaolfi@gmail.com>
2021-04-11 10:04:53 -0300

    add dwm:

---
+++ dwm/install.sh
@@ -0,0 +1,15 @@
+# dwm
+git clone https://git.suckless.org/dwm/
+cd dwm
+make
+make install
+cd -
+rm -f -r dwm
+
+# dwmstatus
+git clone git://git.suckless.org/dwmstatus
+cd dwmstatus
+make
+make install
+cd -
+rm -f -r dwmstatus
